67f41963c95448b58d769a928f9f4a90b61ef97c
Saurav Sabu <saurav.sabu9@gmail.com>
2024-02-21 11:10:37 +0530

    setup.py and requirements.txt code updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="toxic-hate-speech-classification",
+    version="0.0.1",
+    author="Saurav Sabu",
+    author_email="saurav.sabu9@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
