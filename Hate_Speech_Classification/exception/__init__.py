269aedb974ce8bad79740fa672a8b90b2938f420
Saurav Sabu <saurav.sabu9@gmail.com>
2024-02-21 11:27:59 +0530

    exception.py code updated

---
+++ Hate_Speech_Classification/exception/__init__.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+def error_message_detail(error,error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occurred python script name [{filename}] line number [{exc_tb.tb_lineno}] error [{str(error)}]"
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(
+            error_message,error_details
+        )
+
+    def __str__(self):
+        return self.error_message
